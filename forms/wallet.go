6bb949bd3b3ad180365051c7753adb87d4c27aa2
zzggo <han0303006@outlook.com>
2022-07-28 05:33:29 +1000

    fixed: api key

--- forms/wallet.go
+++ forms/wallet.go
@@ -5,7 +5,7 @@ type WalletForm struct{}
 
 //AccountForm ...
 type AccountForm struct {
-	PublicKey string `form:"public_key" json:"public_key" binding:"required"`
-	SignAlgo  int    `form:"sign_algo" json:"sign_algo" binding:"required"`
-	HashAlgo  int    `form:"hash_algo" json:"hash_algo" binding:"required"`
+	PublicKey string `form:"publicKey" json:"publicKey" binding:"required"`
+	SignAlgo  int    `form:"signatureAlgorithm" json:"signatureAlgorithm" binding:"required"`
+	HashAlgo  int    `form:"hashAlgorithm" json:"hashAlgorithm" binding:"required"`
 }
